98714849620a31c39a8dc493e68867fb3e9fb267
Tom Hobbs <tomhobbs@outlook.com>
2021-11-04 16:38:44 +0000

    Styling tidying

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         <h1>
           Sir Jukebox
         </h1>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/hmKshpLXnxE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <p>Under construction by <a href='https://github.com/tommyhobbs'>Tom Hobbs</a></p>
       </header>
+      <body className='main'>
+      <iframe src="https://www.youtube.com/embed/hmKshpLXnxE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </body>
     </div>
   );
 }
