a5caba19ebec4cf4e34cd07b8c92b39090ad2769
Tom Hobbs <tomhobbs@outlook.com>
2021-11-15 22:14:23 +0000

    Open app on notification click

--- client/public/worker.js
+++ client/public/worker.js
@@ -9,4 +9,29 @@ self.addEventListener("push", (e) => {
   });
 });
 
-self.addEventListener("notificationclick", () => clients.openWindow("/"));
+self.addEventListener("notificationclick", (event) => {
+  event.waitUntil(
+    (async function () {
+      const allClients = await clients.matchAll({
+        includeUncontrolled: true,
+      });
+      let sirjukeboxClient;
+      // Let's see if we already have a chat window open:
+      for (const client of allClients) {
+        if (client.location.href.indexOf("sirjukebox") > -1) {
+          // Excellent, let's use it!
+          client.focus();
+          sirjukeboxClient = client;
+          break;
+        }
+      }
+      // If we didn't find an existing chat window,
+      // open a new one:
+      if (!sirjukeboxClient) {
+        sirjukeboxClient = await clients.openWindow(
+          "https://sirjukebox.herokuapp.com/"
+        );
+      }
+    })()
+  );
+});
